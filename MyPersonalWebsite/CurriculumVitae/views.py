945da24873152cffc16df254b7160043d4259687
Gaidysheff <gaidysheff@mail.ru>
2022-10-09 17:42:29 +0500

    Favicon added

--- MyPersonalWebsite/CurriculumVitae/views.py
+++ MyPersonalWebsite/CurriculumVitae/views.py
@@ -1,4 +1,8 @@
+from django.views.generic.base import TemplateView
+from .utilities import DataMixin
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.views import LoginView
 from .utilities import menu_1, menu_2
 
 
@@ -54,3 +58,15 @@ def smth_3(request):
         'title': 'Что-нибудь 3'
     }
     return render(request, 'CurriculumVitae/smth_3.html', context=context)
+
+
+# https://docs.djangoproject.com/en/4.1/ref/class-based-views/base/#view
+
+
+# class Smth_3(DataMixin, TemplateView):
+#     templates_name = 'CurriculumVitae\smth_3.html'
+
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         c_def = self.get_user_context(title="Главная страница")
+#         return dict(list(context.items()) + list(c_def.items()))
