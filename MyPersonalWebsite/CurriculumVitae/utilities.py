5fb405205212f08f7728c617e31857f0c4e76b86
Gaidysheff <gaidysheff@mail.ru>
2022-10-11 21:18:18 +0500

    Main Page finished

--- MyPersonalWebsite/CurriculumVitae/utilities.py
+++ MyPersonalWebsite/CurriculumVitae/utilities.py
@@ -1,6 +1,6 @@
 
 menu_1 = [{'title': "Главная страница", 'url_name': 'home'},
-          {'title': "О сайте", 'url_name': 'about'},
+          {'title': "О разработчике", 'url_name': 'about'},
           {'title': "Обратная связь", 'url_name': 'contact_me'},
           ]
 
